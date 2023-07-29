ca221bdf01f5c4de110bee1ad7649f4cac6d0a6d
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-07-28 20:20:33 -0500

    added prompt questions

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const shapes = require('./lib/shapes');
+
+function questions() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter three characters",
+                name: "text"
+            },
+            {
+                type: "input",
+                message: "What color?",
+                name: "color"
+            },
+            {
+                type: "list",
+                message: "What shape?",
+                choices: ['circle', 'triangle', 'square'],
+                name: "shape"
+            },
+            {
+                type: "input",
+                message: "What color should the shape be?",
+                name: "shape color"
+            },
+        ])
+        .then((data) => {
+            writeToFile('logo.svg',data);
+        })
+};
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created file name')
+    );
+};
